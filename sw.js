54f887712f973ec6ec20554bec5fed14239d2a6e
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-08-19 23:54:46 +0900

    pwa: rotate cache for client quality parity

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ai-remote-finder-v11';
+const CACHE='ai-remote-finder-v12';
 const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg'];
 const INDEX_URL=new URL('./index.html',self.registration.scope).toString();
 const DATA_URL=new URL('./data/jobs.json',self.registration.scope).toString();
