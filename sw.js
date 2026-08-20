e27c5f1ac19d9818da05513775a999d1084daeaf
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-08-20 21:03:55 +0900

    Polish PWA install assets for mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ai-remote-finder-v12';
-const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='ai-remote-finder-v13';
+const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg','./icon-180.png','./icon-192.png','./icon-512.png'];
 const INDEX_URL=new URL('./index.html',self.registration.scope).toString();
 const DATA_URL=new URL('./data/jobs.json',self.registration.scope).toString();
 
